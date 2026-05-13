5509dbe6d895b39e1a3f5a5db145636e7fb8d7a0
Danny Sifukwe <dannysimfukwe@gmail.com>
2026-05-13 06:53:05 +0200

    Fix admin folder detection - use strpos instead of regex

--- cleanup.php
+++ cleanup.php
@@ -17,7 +17,7 @@ $adminRenamed = false;
 $dh = @opendir('/var/www/html');
 if ($dh) {
     while (($f = readdir($dh)) !== false) {
-        if (preg_match('/^admin[a-f0-9]+$/', $f) && is_dir('/var/www/html/' . $f)) {
+        if (strpos($f, 'admin') === 0 && $f !== 'admin' && is_dir('/var/www/html/' . $f)) {
             $adminRenamed = true;
             break;
         }
